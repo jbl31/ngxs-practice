bb76bf8604792ce994fe1b81cff499ab5aafab94
Jean-Baptiste LEOTY <jean-baptiste.leoty@atos.net>
2022-02-07 23:50:22 +0100

    Add general component (wip)

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AddUsers, GetUsers } from '../actions/app.action';
+import { AppState } from '../state/app.state';
 
 @Component({
   selector: 'app-general',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  userInfo: [];
+  @Select(AppState.selectStateData) userInfo$: Observable<any>;
+
+  constructor(private store: Store, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userForm = this.fb.group({
+      id: [''],
+      name: [''],
+      username: [''],
+      email: [''],
+      phone: [''],
+      website: ['']
+    })
+
+    this.store.dispatch(new GetUsers());
+
+    this.userInfo$.subscribe((returnData) => {
+      this.userInfo = returnData;
+    })
+  }
+
+  addUser(){
+    this.store.dispatch(new AddUsers(this.userForm.value));
+    this.userForm.reset();
+  }
+
+  updateUser(id: number, i:any) {
+
+    const newData = {
+      id: id,
+      name: "Siddhesh Thipse",
+      username: "iamsid2399",
+      email: 'siddheshthipse09@gmail.com',
+      phone: '02138-280044',
+      website: 'samplewebsite.com'
+    }
+
+    this.store.dispatch(new UpdateUsers(newData, id, i));
+  }
+
+  deleteUser(i:any) {
+    this.store.dispatch(new DeleteUsers(i));
   }
 
 }
