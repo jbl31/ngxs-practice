0557a908b161a3a9cf45d4c2d6b68af95ddd1e21
Jean-Baptiste LEOTY <jean-baptiste.leoty@atos.net>
2022-02-07 16:43:48 +0100

    Add AppState to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { AppState } from './state/app.state';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,7 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
     FormsModule,
     ReactiveFormsModule,
 
-    NgxsModule.forRoot([]), NgxsLoggerPluginModule.forRoot(), NgxsReduxDevtoolsPluginModule.forRoot()
+    NgxsModule.forRoot([AppState]), NgxsLoggerPluginModule.forRoot(), NgxsReduxDevtoolsPluginModule.forRoot()
   ],
   providers: [DesignUtilityService],
   bootstrap: [AppComponent]
